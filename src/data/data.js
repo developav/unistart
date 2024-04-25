2c6075b2100089a0bdf3e7f8289076d0b3800dba
Dmitry Pavlov <dmitriiy.pavlov@gmail.com>
2024-04-25 16:56:20 +0300

    change way img

--- src/data/data.js
+++ src/data/data.js
@@ -2,41 +2,41 @@ export default [
     {
         id: 1,
         prof:'Копирайтер',
-        image: '../src/img/copy.png',
+        image: '../img/copy.png',
     },
     {
         id: 2,
         prof:'Механик',
-        image: '../src/img/mechanic.png',
+        image: '../img/mechanic.png',
     },
     {
         id: 3,
         prof:'Шеф-повар',
-        image: '../src/img/chef.png',
+        image: '../img/chef.png',
     },
     {
         id: 4,
         prof:'Адвокат',
-        image: '../src/img/loer.png',
+        image: '../img/loer.png',
     },
     {
         id: 5,
         prof:'Юрист',
-        image: '../src/img/loer.png',
+        image: '../img/loer.png',
     },
     {
         id: 6,
         prof:'Техник',
-        image: '../src/img/mechanic.png',
+        image: '../img/mechanic.png',
     },
     {
         id: 7,
         prof:'Разработчик',
-        image: '../src/img/loer.png',
+        image: '../img/loer.png',
     },
     {
         id: 8,
         prof:'Технолог',
-        image: '../src/img/mechanic.png',
+        image: '../img/mechanic.png',
     },
 ]
