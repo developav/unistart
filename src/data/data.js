96e98649b01fbb3e223a396b7e5056a353d5e0ad
developav <joker89sn@icloud.com>
2024-04-25 22:14:38 +0300

    change correct way image

--- src/data/data.js
+++ src/data/data.js
@@ -7,36 +7,36 @@ export default [
     {
         id: 2,
         prof:'Механик',
-        image: 'img/mechanic.png',
+        image: 'src/img/mechanic.png',
     },
     {
         id: 3,
         prof:'Шеф-повар',
-        image: 'img/chef.png',
+        image: 'src/img/chef.png',
     },
     {
         id: 4,
         prof:'Адвокат',
-        image: 'img/loer.png',
+        image: 'src/img/loer.png',
     },
     {
         id: 5,
         prof:'Юрист',
-        image: 'img/loer.png',
+        image: 'src/img/loer.png',
     },
     {
         id: 6,
         prof:'Техник',
-        image: 'img/mechanic.png',
+        image: 'src/img/mechanic.png',
     },
     {
         id: 7,
         prof:'Разработчик',
-        image: 'img/loer.png',
+        image: 'src/img/loer.png',
     },
     {
         id: 8,
         prof:'Технолог',
-        image: 'img/mechanic.png',
+        image: 'src/img/mechanic.png',
     },
 ]
