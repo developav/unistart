77dc918fc0ec5678d2902583f18a18a147d66772
developav <joker89sn@icloud.com>
2024-04-25 22:05:29 +0300

    change way img

--- src/data/data.js
+++ src/data/data.js
@@ -2,41 +2,41 @@ export default [
     {
         id: 1,
         prof:'Копирайтер',
-        image: '../img/copy.png',
+        image: 'img/copy.png',
     },
     {
         id: 2,
         prof:'Механик',
-        image: '../img/mechanic.png',
+        image: 'img/mechanic.png',
     },
     {
         id: 3,
         prof:'Шеф-повар',
-        image: '../img/chef.png',
+        image: 'img/chef.png',
     },
     {
         id: 4,
         prof:'Адвокат',
-        image: '../img/loer.png',
+        image: 'img/loer.png',
     },
     {
         id: 5,
         prof:'Юрист',
-        image: '../img/loer.png',
+        image: 'img/loer.png',
     },
     {
         id: 6,
         prof:'Техник',
-        image: '../img/mechanic.png',
+        image: 'img/mechanic.png',
     },
     {
         id: 7,
         prof:'Разработчик',
-        image: '../img/loer.png',
+        image: 'img/loer.png',
     },
     {
         id: 8,
         prof:'Технолог',
-        image: '../img/mechanic.png',
+        image: 'img/mechanic.png',
     },
 ]
